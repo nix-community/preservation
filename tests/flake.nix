b103724458990c0566139bb17d81289fa6e35351
Marie Ramlow <me@nycode.dev>
2025-02-11 17:27:12 +0100

    tests: also run tests on stable

--- tests/flake.nix
+++ tests/flake.nix
@@ -3,6 +3,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
   outputs = inputs:
@@ -13,13 +14,27 @@
           "x86_64-linux"
           "aarch64-linux"
         ];
-        perSystem = { pkgs, ... }:
-          {
-            checks = {
-              default = pkgs.nixosTest (import ./basic.nix pkgs);
-              firstboot = pkgs.nixosTest (import ./firstboot.nix pkgs);
-              verity-image = pkgs.nixosTest (import ./appliance-image-verity.nix pkgs);
-            };
+      perSystem =
+        {
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          checksForPkgs = pkgs: {
+            default = pkgs.nixosTest (import ./basic.nix pkgs);
+            firstboot = pkgs.nixosTest (import ./firstboot.nix pkgs);
+            verity-image = pkgs.nixosTest (import ./appliance-image-verity.nix pkgs);
           };
-      };
+          stableChecks = (
+            lib.mapAttrs' (n: lib.nameValuePair "${n}-stable") (
+              checksForPkgs inputs.nixpkgs-stable.legacyPackages.${system}
+            )
+          );
+        in
+        {
+          checks = checksForPkgs pkgs // stableChecks;
+        };
+    };
 }
